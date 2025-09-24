4e6f8c5ec1263a5cc13fc3b06c3282bc2a32d7a3
HamzaLachiq <lachiq.hamza37@gmail.com>
2025-09-24 11:35:39 +0100

    first commit

---
+++ src/main/java/T360/Calenderservice/CalenderserviceApplication.java
@@ -0,0 +1,13 @@
+package T360.Calenderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalenderserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalenderserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/T360/Calenderservice/CalenderserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package T360.Calenderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalenderserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
